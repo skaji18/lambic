c2f102ebfb37ecb781e75ef4b34382c15571dfab
skaji18 <kasuke18@gmail.com>
2021-08-20 22:26:40 +0900

    TypeScript対応

---
+++ src/types/User.ts
@@ -0,0 +1,6 @@
+export type User = {
+    id: String
+    name: String
+    photoURL: String
+    isAdmin: Boolean
+}
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   chainWebpack: config => {
-    config.module
-      .rule('firebase')
+    // firebase-loader の設定
+    config.module.rule('firebase')
       .test(/firebase-config\.json$/)
       .use('firebase-config')
       .loader('./firebase-config-loader')
+      .end()
+    // ts-loader の設定
+    config.module.rule('type-script')
+      .test(/\.tsx?$/)
+      .use('ts-loader')
+      .loader('ts-loader')
+      .options({ transpileOnly: true, appendTsSuffixTo: [/\.vue$/] })
+      .end()
   },
   pages: {
     index: {
