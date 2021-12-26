4c4ec7dc7a6e53337c4ee9f730e066ceaba06a14
skaji18 <kasuke18@gmail.com>
2021-12-26 14:16:50 +0900

    vuetify プラグインのインストール方法変更

--- src/main.js
+++ src/main.js
@@ -2,23 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify'
+import { vuetify } from './vuetify'
 import VueQriously from 'vue-qriously'
 import moment from 'moment'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'vuetify/dist/vuetify.min.css'
 // import './registerServiceWorker'
 
 moment.locale('ja', {
   weekdaysShort: ['日', '月', '火', '水', '木', '金', '土']
 })
 
-Vue.use(Vuetify)
 Vue.use(VueQriously)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
