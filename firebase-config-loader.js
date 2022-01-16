80896306c6373e75d7774877b00cad85184530dc
skaji18 <kasuke18@gmail.com>
2022-01-16 17:37:43 +0900

    firebase のバージョン更新
    * emulator を用いたe2eテストの事前準備

--- firebase-config-loader.js
+++ firebase-config-loader.js
@@ -2,7 +2,16 @@ const firebaseTools = require('firebase-tools')
 
 module.exports = function FirebaseConfigLoader () {
   const callback = this.async()
-  firebaseTools.setup.web().then(config => {
-    callback(null, JSON.stringify(config))
-  }).catch(err => callback(err))
+  firebaseTools
+    .use(null, { projectRoot: __dirname })
+    .then((projectId) => firebaseTools.apps.list('web', { project: projectId }))
+    .then((apps) =>
+      firebaseTools.apps.sdkconfig('web', apps[0].appId, {
+        project: apps[0].projectId
+      })
+    )
+    .then((config) => {
+      callback(null, JSON.stringify(config.sdkConfig))
+    })
+    .catch((err) => callback(err))
 }
